2122cc5be00287856fbb30b97a8e2d8de51f52b3
IsaiahSama <zeldaplayergl@gmail.com>
2025-03-10 20:59:01 -0400

    Added a scheduler to decay the learning rate of each parameter

--- step_3_train.py
+++ step_3_train.py
@@ -77,13 +77,16 @@ def main():
     net = Net().float()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
-
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    
     trainloader, _ = get_train_test_loaders()
 
     # An epoch is an iteration of training where every training sample
     # has been used exactly once.
     for epoch in range(2):
         train(net, criterion, optimizer, trainloader, epoch)
+        scheduler.step()
+
     torch.save(net.state_dict(), "checkpoint.pth")
 
 
